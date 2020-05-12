c10f1c90a1ba40f5cc812a18c61e72fd48759ff3
Plinio Prado <plinioprado01@gmail.com>
2020-05-12 09:01:52 -0700

    pre webpack breaking of earth dependency file

---
+++ src/earth.js
@@ -0,0 +1,24 @@
+document.onLoad = loadApp;
+
+function getEarth() {
+  var id = 'earth';
+  var state = {
+    width: 12756200, // (m)
+    height: 12756200, // (m)
+    g: 9.80665, // m/s2
+    position: {
+      r: 0, // distance from center (m)
+      dec: 0, // not used declination (deg)
+    },
+    style: {
+      borderRadius: '50%', // to circle
+      backgroundColor: 'blue',
+      zIndex: 3
+    }
+  }
+
+  return {
+    id,
+    state
+  }
+}
