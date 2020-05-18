1a86aef43b51393e17c4441e802e7060b366318a
Plinio Prado <plinioprado01@gmail.com>
2020-05-18 10:55:31 -0700

    svg ship rotating

--- src/ship.js
+++ src/ship.js
@@ -1,63 +1,80 @@
-let ship1 = [
-  {
-		id: 'ship1',
-		render: {
-			format: 'g',
-			transform: 'translate(200,200) rotate(0)'
-		}
-	},
-	{
-		id: 'ship-blur',
-		render: {
-			parentId: 'ship1',
-			format: 'filter',
-		}
-  },
-	{
-		render: {
-			parentId: 'ship-blur',
-			format: 'feGaussianBlur',
-			stdDeviation: "1.5"
-		}
-	},
-	{
-		id: 'ship-crop',
-		render: {
-			parentId: 'ship1',
-			format: 'clipPath',
-		}
-	},
-	{
-		render: {
-			parentId: 'ship-crop',
-			format: 'rect',
-			x: 0,
-			y: 8,
-			width:10,
-			height: 17
-		}
-	},
-	{
-		render: {
-			parentId: 'ship1',
-			format: 'ellipse',
-			cx: 5,
-			cy: 23,
-			rx:5,
-			ry: '12',
-			color: 'red',
-			clip: 'url(#ship-crop)',
-			filter: 'url(#ship-blur)'
-		}
-	},
-	{
-		render: {
-			parentId: 'ship1',
-			format: 'polygon',
-			points: '5,0 10,10 5,7.5 0,10',
-			color: 'white'
-		}
-	}
-]
+let ship1 = () => {
+
+  const objList = [
+    {
+      id: 'ship1',
+      position: {
+        r: 6378100, // distance (m)
+        dec: 0, // declination (deg)
+        pitchDec: 0, // attitude pitch (deg)
+      },
+      render: {
+        format: 'g',
+        transform: 'translate(200,200) rotate(0)'
+      }
+    },
+    {
+      id: 'shipblur',
+      render: {
+        parentId: 'ship1',
+        format: 'filter',
+      }
+    },
+    {
+      render: {
+        parentId: 'shipblur',
+        format: 'feGaussianBlur',
+        stdDeviation: "1.5"
+      }
+    },
+    {
+      id: 'shipcrop',
+      render: {
+        parentId: 'ship1',
+        format: 'clipPath',
+      }
+    },
+    {
+      render: {
+        parentId: 'shipcrop',
+        format: 'rect',
+        x: 0,
+        y: 8,
+        width:10,
+        height: 17
+      }
+    },
+    {
+      render: {
+        parentId: 'ship1',
+        format: 'ellipse',
+        cx: 5,
+        cy: 23,
+        rx:5,
+        ry: '12',
+        color: 'red',
+        clipPath: 'url(#shipcrop)',
+        filter: 'url(#shipblur)'
+      }
+    },
+    {
+      render: {
+        parentId: 'ship1',
+        format: 'polygon',
+        points: '5,0 10,10 5,7.5 0,10',
+        color: 'white'
+      }
+    }
+  ]
+
+  const addPitch = (add) => {
+    objList[0].position.pitchDec = (objList[0].position.pitchDec += add) % 360;
+  }
+  
+  return {
+    objList,
+    addPitch
+  }
+}
 
 export default ship1;
