6060edc1104205a2111fb1f40cc7222203efca3e
Plinio Prado <plinioprado01@gmail.com>
2020-06-07 22:15:02 -0700

    most panel data in state

--- src/panel.js
+++ src/panel.js
@@ -4,47 +4,6 @@ let getPanel = (helpCalc, state, ship1) => {
   var position = {};
   var panel = {}
   var content = {
-    alt: function() {
-      var alt = state.render.refId === 'earth' ? panel.altEarth : panel.altMoon;
-      var unit = 'm';
-      if (alt > 1000) {
-        alt /=  1000
-        unit = 'km';
-      }
-      return Math.round(alt).toLocaleString('en-US') + unit;
-    },
-    long: function() {
-      var long = state.render.refId === 'earth' ? panel.headEarth : panel.headMoon;
-      return formatLong((180  - long) % 360);
-    },
-    pitch: function() {
-      var pitch = helpCalc.toDeg360(position.pitchDec - position.dec)
-      return formatDeg(helpCalc.toDeg180(90 - pitch ))
-    },
-    climb: function() {
-      var vDec = position.vDec + position.dec;
-      var climb = position.vR * Math.cos(vDec * (Math.PI/180))
-      return Math.round(climb * 3.6).toLocaleString('en-US') + 'km/h';
-    },
-    vOrbit: function() {
-      var vDec = position.vDec + position.dec;
-      var v = position.vR * Math.sin(vDec * (Math.PI/180))
-      return Math.round(v * 3.6).toLocaleString('en-US') + 'km/h';
-    },
-    gLocal: function() {
-      return (state.render.refId === 'earth' ? panel.gEarth : panel.gMoon).toFixed(2) + 'm/s2';
-    },
-    speed: function() {
-      return Math.round(position.vR * 3.6).toLocaleString('en-US') + 'km/h';
-    },
-    burstA: function() {
-      var a = (position.burst.a / 9.8).toFixed(0);
-      var aNext = (position.burst.aNext / 9.8).toFixed(0);
-      return a + 'g (' + aNext + 'g)';
-    },
-    burstT: function() {
-      return Math.round(position.burst.t) + 's (' + position.burst.tNext.toFixed(0) + 's)';
-    },
     scale: function() {
       var scale = state.width / 10  * state.render.zoom;
       return formatMkm(scale);g
@@ -56,9 +15,6 @@ let getPanel = (helpCalc, state, ship1) => {
       var days = parseInt((d - d0) / (1000 * 60 * 60 * 24));
       return days + 'd ' + d.toLocaleTimeString('en-US', { hour12: false });
     },
-    head: function() {
-      return formatDeg(position.vDec);
-    },
     zoom: function() {
       var zoom  = state.render.zoom
       return zoom < 1000 ? zoom : Math.round(zoom / 1000) + 'k';
@@ -68,9 +24,6 @@ let getPanel = (helpCalc, state, ship1) => {
     },
     timePlay: function() {
       return state.play ? 'Pause' : 'Play';
-    },
-    ref: function() {
-      return state.render.refId;
     }
   }
 
@@ -78,6 +31,25 @@ let getPanel = (helpCalc, state, ship1) => {
 
   function create(state) {
     state.panel = {
+      shared: {
+        node: null
+      },
+      thrust: {
+        thrust: () => {
+          var a = (ship1.position.burst.a / 9.8).toFixed(0);
+          var aNext = (ship1.position.burst.aNext / 9.8).toFixed(0);
+          return a + 'g (' + aNext + 'g)';
+        },
+        time: () => position.burst.t.toFixed(0) + 's (' + position.burst.tNext.toFixed(0) + 's)'
+      },
+      FDAI: {
+        speed: () => (ship1.position.vR * 3.6).toFixed(0).toLocaleString() + 'km/h',
+        head: () => formatDeg(ship1.position.vDec),
+        pitch: () => {
+          var pitch = helpCalc.toDeg360(ship1.position.pitchDec - ship1.position.dec)
+          return formatDeg(helpCalc.toDeg180(90 - pitch ))
+        }
+      },
       nav2: {
         ref: 'earth',
         dec: (state) => {
@@ -96,11 +68,43 @@ let getPanel = (helpCalc, state, ship1) => {
           const dist = helpCalc.vectorSub(postShip, posNavRef);
           return formatMkm(dist.r - refObj.r);
         }
+      },
+      nav: {
+        ref: (state) => state.render.refId,
+        alt: (state) => {
+          const posShip = {r: ship1.position.r, dec: ship1.position.dec};
+          const refObj = state.render.refObjs[state.render.refId];
+          const posRef = {r: refObj.position.r, dec: refObj.position.dec};
+          const dist = helpCalc.vectorSub(posShip, posRef);
+          return formatMkm(dist.r - refObj.r);
+        },
+        long: () => {
+          return formatLong(ship1.position.dec);
+        },
+        vClimb: (state) => {
+          //const refObj = state.render.refObjs[state.render.refId];
+          const climb = ship1.position.vR * Math.cos(ship1.position.vDec * (Math.PI/180));
+          return (climb * 3.6).toFixed(0) + 'km/h'
+        },
+        vOrb: (state) => {
+          //const refObj = state.render.refObjs[state.render.refId];
+          const vOrb = ship1.position.vR * Math.sin(ship1.position.vDec * (Math.PI/180));
+          return (vOrb * 3.6).toFixed(0) + 'km/h'
+        },
+        gLocal: (state) => {
+          const posShip = {r: ship1.position.r, dec: ship1.position.dec};
+          const navRefId = state.render.refId;
+          const refObj = state.render.refObjs[navRefId];
+          const posNavRef = {r: refObj.position.r, dec: refObj.position.dec};
+          const dist = helpCalc.vectorSub(posShip, posNavRef);
+          const gLocal = helpCalc.getGLocalR(dist.r, refObj.mass);
+          return gLocal.toFixed(2) + 'm/s2';
+        }
       }
     };
 
-    state.panel.node = document.getElementById('panel');
-    createNav2(state);
+    state.panel.shared.node = document.getElementById('panel');
+    createSections(state);
   }
 
   function update(key, state) {
@@ -126,7 +130,7 @@ let getPanel = (helpCalc, state, ship1) => {
       // from state
       var keys1 = Object.keys(state.panel);
       keys1.forEach(function(key1) {
-        if (key1 !== 'node') {
+        if (key1 !== 'shared') {
           var keys2 = Object.keys(state.panel[key1]);
           keys2.forEach(function(key2) {
             const value = state.panel[key1][key2];
@@ -162,36 +166,36 @@ let getPanel = (helpCalc, state, ship1) => {
     return txt;
   }
 
-  function createNav2(state) {
-    const label = 'Nav2'
-    let sectionNode = createSection(label);
-    var keys = Object.keys(state.panel.nav2);
-    keys.forEach(function(key) {
-      createInstrument(sectionNode, 'nav2', key);
+  function createSections(state) {
+    const sectionKeys = Object.keys(state.panel);
+    sectionKeys.forEach((sectionKey) => {
+      if (sectionKey !== 'shared') {
+        const sectionNode = getSection(sectionKey);
+        const instrKeys = Object.keys(state.panel[sectionKey]);
+        instrKeys.forEach((instrKey) => {
+          const instrNode = getInstruments(state.panel[sectionKey][instrKey], sectionKey, instrKey)
+          sectionNode.appendChild(instrNode);
+        })
+        state.panel.shared.node.prepend(sectionNode);
+      }
     });
-    state.panel.node.appendChild(sectionNode);
-    keys.forEach(function(key) {
-      if (key !== 'node') update('nav2.' + key, state);
-    });
-  }
 
-  function createSection(label) {
-    let node = document.createElement("section");
+    function getSection(sectionKey) {
+      let node = document.createElement("section");
+      const labelNode = document.createElement("label");
+      labelNode.innerText = sectionKey;
+      node.appendChild(labelNode);
+      return node;
+    }
 
-    const labelNode = document.createElement("label");
-    labelNode.innerText = label;
-    node.appendChild(labelNode);
-
-    return node;
-  }
-
-  function createInstrument(sectionNode, sectionId, instrId) {
-    const instrNode = document.createElement("div");
-    instrNode.innerText = instrId + ': ';
-    const instrSpanNode = document.createElement("span");
-    instrSpanNode.id = 'panel_' + sectionId + '_' + instrId;
-    instrNode.appendChild(instrSpanNode);
-    sectionNode.appendChild(instrNode);
+    function getInstruments(instrState, sectionKey, instrKey) {
+      const instrNode = document.createElement("div");
+      instrNode.innerText = instrKey + ': ';
+      const instrSpanNode = document.createElement("span");
+      instrSpanNode.id = 'panel_' + sectionKey + '_' + instrKey;
+      instrNode.appendChild(instrSpanNode);
+      return instrNode;
+    }
   }
 
   function changeNav2Ref(state) {
